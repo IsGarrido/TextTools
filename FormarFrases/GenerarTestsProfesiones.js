a4989e964e8c40d3469d111121a48450c8c2ef5f
Ismael Garrido Muñoz <ismaelgarridomunoz@gmail.com>
2021-07-26 10:49:27 +0200

    save

--- FormarFrases/GenerarTestsProfesiones.js
+++ FormarFrases/GenerarTestsProfesiones.js
@@ -13,6 +13,8 @@ let uid = 0;
 let pId = 0;
 const results = [];
 
+
+// Test clasico 0
 let plantilla = plantillas[0];
 for (const genero of generos) {
   for (const profesion of profesiones) {
@@ -21,20 +23,70 @@ for (const genero of generos) {
       continue;
     
     const s1 = Utils.FastInterpolate(plantilla, [['genero', genero[0]], ['profesion', profesion[0]]] ); // El es profesor
-    const s2 = Utils.FastInterpolate(plantilla, [['genero', genero[1]], ['profesion', profesion[1]]] ); // El es profesor
-    const m1 = Utils.FastInterpolate(plantilla, [['genero', genero[0]], ['profesion', BERT.MASK]] ); // El es profesor
-    const m2 = Utils.FastInterpolate(plantilla, [['genero', genero[1]], ['profesion', BERT.MASK]] ); // El es profesor
+    const s2 = Utils.FastInterpolate(plantilla, [['genero', genero[1]], ['profesion', profesion[1]]] ); // Ella es profesora
+    const m1 = Utils.FastInterpolate(plantilla, [['genero', genero[0]], ['profesion', BERT.MASK]] ); // El es [MASK]
+    const m2 = Utils.FastInterpolate(plantilla, [['genero', genero[1]], ['profesion', BERT.MASK]] ); // Ella es [MASK]
 
-    const items = [s1, s2, m1, m2, ...profesion, uid++, pId, '', '', '', '', '', '', '', ''];
+    const items = [s1, s2, m1, m2, ...profesion, uid++, pId, '', '', '', '', '', '', '', '', '', ''];
     results.push(items.join('\t'));
   }
 }
 
 
+// Test avanzado 1
+
 pId++;
 plantilla = plantillas[1];
 
+for (const profesion of profesiones) {
+  for ( const determinante of determinantes) {
+    for ( const nombre of nombres) {
+
+      if(profesion[0].includes(" ")) // Temporal?
+        continue;
+
+      const s1 = Utils.FastInterpolate(plantilla, [['nombre', nombre[0]], ['determinante', determinante[0]], ['profesion', profesion[0]]] ); // El es profesor
+      const s2 = Utils.FastInterpolate(plantilla, [['nombre', nombre[1]], ['determinante', determinante[1]], ['profesion', profesion[1]]] ); // Ella es profesora
+      const m1 = Utils.FastInterpolate(plantilla, [['nombre', nombre[0]], ['determinante', determinante[0]], ['profesion', BERT.MASK]] ); // El es [MASK]
+      const m2 = Utils.FastInterpolate(plantilla, [['nombre', nombre[1]], ['determinante', determinante[1]], ['profesion', BERT.MASK]] ); // Ella es [MASK]
+
+      const items = [s1, s2, m1, m2, ...profesion, uid++, pId, ...nombre, ...determinante, '', '', ...profesion, '', ''];
+      results.push(items.join('\t'));
+    }
+  }
+}
+
+
+
+
+// Log odds
+// Enmascarar todo el genero, basico 2
+pId++;
+plantilla = plantillas[0];
 for (const genero of generos) {
+  for (const profesion of profesiones) {
+    
+    if(profesion[0].includes(" ")) // Temporal?
+      continue;
+    
+    const s1 = Utils.FastInterpolate(plantilla, [['genero', genero[0]], ['profesion', profesion[0]]] ); // El es profesor
+    const s2 = Utils.FastInterpolate(plantilla, [['genero', genero[1]], ['profesion', profesion[1]]] ); // Ella es profesor
+    const m1 = Utils.FastInterpolate(plantilla, [['genero', genero[0]], ['profesion', BERT.MASK]] ); // El es [MASK]          ? profesor
+    const m2 = Utils.FastInterpolate(plantilla, [['genero', genero[1]], ['profesion', BERT.MASK]] ); // Ella es [MASK]        ? profesora
+    const g1 = Utils.FastInterpolate(plantilla, [['genero', BERT.MASK], ['profesion', BERT.MASK]] ); // [MASK] es [MASK]      ? profesor
+    const g2 = Utils.FastInterpolate(plantilla, [['genero', BERT.MASK], ['profesion', BERT.MASK]] ); // [MASK] es [MASK]      ? profesora
+
+    const items = [s1, s2, m1, m2, ...profesion, uid++, pId, '', '', '', '', '', '', '', '', g1, g2];
+    results.push(items.join('\t'));
+  }
+}
+
+
+// Enmascarar todo el avanzado, basico 3
+
+pId++;
+plantilla = plantillas[1];
+
   for (const profesion of profesiones) {
     for ( const determinante of determinantes) {
       for ( const nombre of nombres) {
@@ -42,17 +94,19 @@ for (const genero of generos) {
         if(profesion[0].includes(" ")) // Temporal?
           continue;
   
-        const s1 = Utils.FastInterpolate(plantilla, [['nombre', nombre[0]], ['determinante', determinante[0]], ['genero', genero[0]], ['profesion', profesion[0]]] ); // El es profesor
-        const s2 = Utils.FastInterpolate(plantilla, [['nombre', nombre[1]], ['determinante', determinante[1]], ['genero', genero[1]], ['profesion', profesion[1]]] ); // El es profesor
-        const m1 = Utils.FastInterpolate(plantilla, [['nombre', nombre[0]], ['determinante', determinante[0]], ['genero', genero[0]], ['profesion', BERT.MASK]] ); // El es profesor
-        const m2 = Utils.FastInterpolate(plantilla, [['nombre', nombre[1]], ['determinante', determinante[1]], ['genero', genero[1]], ['profesion', BERT.MASK]] ); // El es profesor
+        const s1 = Utils.FastInterpolate(plantilla, [['nombre', nombre[0]], ['determinante', determinante[0]], ['profesion', profesion[0]]] ); // El caballero es profesor
+        const s2 = Utils.FastInterpolate(plantilla, [['nombre', nombre[1]], ['determinante', determinante[1]], ['profesion', profesion[1]]] ); // La dama es profesora
+        const m1 = Utils.FastInterpolate(plantilla, [['nombre', nombre[0]], ['determinante', determinante[0]], ['profesion', BERT.MASK]] ); // El caballero es [MASK]     ? profesora
+        const m2 = Utils.FastInterpolate(plantilla, [['nombre', nombre[1]], ['determinante', determinante[1]], ['profesion', BERT.MASK]] ); // La dama es [MASK]          ? profesor
+        const g1 = Utils.FastInterpolate(plantilla, [['nombre', BERT.MASK], ['determinante', BERT.MASK], ['profesion', BERT.MASK]] ); // [MASK] [MASK] es [MASK]          ? profesor
+        const g2 = Utils.FastInterpolate(plantilla, [['nombre', BERT.MASK], ['determinante', BERT.MASK], ['profesion', BERT.MASK]] ); // [MASK] [MASK] es [MASK]          ? profesora
 
-        const items = [s1, s2, m1, m2, ...profesion, uid++, pId, ...nombre, ...determinante, ...genero, ...profesion];
+        const items = [s1, s2, m1, m2, ...profesion, uid++, pId, ...nombre, ...determinante, '', '', ...profesion, g1, g2];
         results.push(items.join('\t'));
       }
     }
   }
-}
+
 
 
 Utils.WriteLines('./FormarFrases/tests/profesiones.test.tsv', results);
